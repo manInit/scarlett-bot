2c7dace0962e9857b3182b2c2ba4db4a0c8351f4
manInit <info.denisbronnikov@gmail.com>
2024-07-21 19:54:09 +0800

    test

--- src/commands/music.commands.ts
+++ src/commands/music.commands.ts
@@ -2,9 +2,12 @@ import ICommand from './command.interface';
 import {
   createAudioPlayer,
   createAudioResource,
+  generateDependencyReport,
   joinVoiceChannel,
+  NoSubscriberBehavior,
 } from '@discordjs/voice';
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import fs from 'fs';
 import play from 'play-dl';
 
 const musicCommand: ICommand = {
@@ -19,6 +22,7 @@ const musicCommand: ICommand = {
     }),
 
   async execute(interaction: CommandInteraction): Promise<void> {
+    console.log(generateDependencyReport());
     const member = interaction.guild?.members.cache.get(
       interaction.member!.user.id!
     );
@@ -35,14 +39,24 @@ const musicCommand: ICommand = {
     if (!url) {
       return;
     }
-
-    const stream = await play.stream(url);
-    const player = createAudioPlayer();
-    const resource = createAudioResource(stream.stream, {
+    const yt_info = await play.video_info(url);
+    const stream = await play.stream_from_info(yt_info);
+    const resourceTR = createAudioResource(stream.stream, {
       inputType: stream.type,
     });
+    // const resource = createAudioResource(join(__dirname, 'test.wav'));
+    const outputStream = fs.createWriteStream('audio.ogg');
+    stream.stream.pipe(outputStream as any);
+    outputStream.on('finish', () => {
+      console.log('The audio data has been successfully saved to the file.');
+    });
 
-    player.play(resource);
+    const player = createAudioPlayer({
+      behaviors: {
+        noSubscriber: NoSubscriberBehavior.Play,
+      },
+    });
+    player.play(resourceTR);
     connection.subscribe(player);
 
     if (interaction.isRepliable()) {
